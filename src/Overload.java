23e01222bdf5bfea900e3a6bda6c0ff62abebd7c
almacode1703 <testmygitt.2023@gmail.com>
2026-02-11 21:23:58 +0530

    updates

---
+++ src/Overload.java
@@ -0,0 +1,56 @@
+class Parent {
+    String name;
+    Parent(String name){
+        this.name = name;
+    }
+    public String Display(){
+        return "Name : "+this.name;
+    }
+}
+
+class Child1 extends Parent{
+    String name;
+    int age;
+    Child1( String name, int age){
+        super(name);
+        this.age = age;
+    }
+
+    @Override
+    public String Display() {
+        return "Name : "+this.name+ " Age : "+this.age;
+    }
+
+    public String Fun(){
+        return "Overload works in JAVA";
+    }
+}
+
+class Child2 extends Parent{
+    String name;
+    int age;
+    boolean isAdult;
+    Child2( String name, int age, boolean isAdult){
+        super(name);
+        this.age = age;
+        this.isAdult = isAdult;
+    }
+
+    @Override
+    public String Display() {
+        return "Name : "+this.name+ " Age : "+this.age+ " Is an Adult : "+this.isAdult;
+    }
+
+    public String Fun(){
+        return "Overload works in JAVA";
+    }
+}
+
+public class Overload {
+    public static void main(String[] args) {
+        Parent p1 = new Child1("Jyotirmoy", 28);
+        Parent p2 = new Child2("Jyotirmoy", 28, false);
+        System.out.println(p1.Display());
+        System.out.println(p2.Display());
+    }
+}
