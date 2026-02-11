80d9ad218a813881519c2e18a2e77e14aa6d29a0
almacode1703 <testmygitt.2023@gmail.com>
2026-02-11 22:37:31 +0530

    updates

---
+++ src/GetSet.java
@@ -0,0 +1,19 @@
+class TestBean {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class GetSet {
+    public static void main(String[] args) {
+        TestBean t1 = new TestBean();
+        t1.setName("Codingal");
+        System.out.println(t1.getName());
+    }
+}
